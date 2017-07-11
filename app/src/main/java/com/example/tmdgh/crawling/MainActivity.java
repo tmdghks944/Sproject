bbaa821c0f262f8037aee8523ff91cdb6eb6b4f8
tmdghks944 <tmdghks944@naver.com>
2017-07-11 22:39:30 +0900

    first

---
+++ app/src/main/java/com/example/tmdgh/crawling/MainActivity.java
@@ -0,0 +1,316 @@
+package com.example.tmdgh.crawling;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+
+
+public class MainActivity extends AppCompatActivity {
+    private Camera camera;
+    private int cameraId = 0;
+    private Button btn1;
+    private Button btn2;
+    private static final String TAG = "MainActivity";
+    private TextView resultTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = (Button)findViewById(R.id.camerabutton);
+        btn2 = (Button)findViewById(R.id.imagebutton);
+        resultTextView = (TextView)findViewById(R.id.resultText);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                capture(v);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                sendImg(v);
+            }
+        });
+    }
+
+    public void capture(View view){
+        if (!getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            Toast.makeText(this, "No camera on this device", Toast.LENGTH_LONG)
+                    .show();
+        } else {
+            cameraId = findFrontFacingCamera();
+            if (cameraId < 0) {
+                Toast.makeText(this, "No front facing camera found.",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra("android.intent.extras.CAMERA_FACING", 1);
+                startActivityForResult(intent,0);
+            }
+        }
+    }
+
+    public void sendImg(View view){
+        Intent intent = new Intent();
+        if (Build.VERSION.SDK_INT >= 19) {
+            // For Android versions of KitKat or later, we use a
+            // different intent to ensure
+            // we can get the file path from the returned intent URI
+            intent.setAction(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+        } else {
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+        }
+        intent.setType("image/*");
+        startActivityForResult(intent, 0);
+    }
+
+    private int findFrontFacingCamera() {
+        int cameraId = -1;
+        // Search for the front facing camera
+        int numberOfCameras = Camera.getNumberOfCameras();
+        for (int i = 0; i < numberOfCameras; i++) {
+            Camera.CameraInfo info = new Camera.CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                cameraId = i;
+                break;
+            }
+        }
+        return cameraId;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            Uri uri = data.getData();
+            try {
+                String path = getPath(uri);
+                Toast.makeText(MainActivity.this,path,Toast.LENGTH_SHORT).show();
+                new JSONTask().execute("https://openapi.naver.com/v1/vision/celebrity",path);
+            } catch (URISyntaxException e) {
+                Toast.makeText(this,
+                        "Unable to get the file from the given URI.  See error log for details",
+                        Toast.LENGTH_LONG).show();
+                Log.e(TAG, "Unable to upload file from the given uri", e);
+            }
+        }
+    }
+
+    public class JSONTask extends AsyncTask<String, String, String[]  >{
+        ProgressDialog asyncDialog = new ProgressDialog(
+                MainActivity.this);
+        @Override
+        protected  void onPreExecute(){
+            asyncDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            asyncDialog.setMessage("검색중입니다 잠시만 기다려 주세요.");
+
+            asyncDialog.show();
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String[] doInBackground(String... params) {
+            StringBuffer reqStr = new StringBuffer();
+            
+            try {
+                String paramName = "image";
+                String imgFile = params[1];
+                File uploadFile = new File(imgFile);
+
+                URL url = new URL(params[0]);
+                HttpURLConnection con = (HttpURLConnection)url.openConnection();
+                con.setUseCaches(false);
+                con.setDoOutput(true);
+                con.setDoInput(true);
+
+                // multipart request
+                String boundary = "---" + System.currentTimeMillis() + "---";
+                con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+                con.setRequestProperty("X-Naver-Client-Id", clientId);
+                con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+
+                OutputStream outputStream = con.getOutputStream();
+
+                PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream, "UTF-8"), true);
+                String LINE_FEED = "\r\n";
+
+                // file 추가
+                String fileName = uploadFile.getName();
+                writer.append("--" + boundary).append(LINE_FEED);
+                writer.append("Content-Disposition: form-data; name=\"" + paramName + "\"; filename=\"" + fileName + "\"").append(LINE_FEED);
+                writer.append("Content-Type: "  + URLConnection.guessContentTypeFromName(fileName)).append(LINE_FEED);
+                writer.append(LINE_FEED);
+                writer.flush();
+                FileInputStream inputStream = new FileInputStream(uploadFile);
+                byte[] buffer = new byte[4096];
+                int bytesRead = -1;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+                outputStream.flush();
+                inputStream.close();
+                writer.append(LINE_FEED).flush();
+                writer.append("--" + boundary + "--").append(LINE_FEED);
+                writer.close();
+                BufferedReader br = null;
+                int responseCode = con.getResponseCode();
+                if(responseCode==200) { // 정상 호출
+                    br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                } else {  // 에러 발생
+                    System.out.println("error!!! responseCode= " + responseCode);
+                    br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                }
+                String inputLine;
+                if(br != null) {
+                    StringBuffer response = new StringBuffer();
+                    while ((inputLine = br.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+                    br.close();
+                    String result = response.toString();
+                    System.out.println(result);
+                    JSONObject jObject = new JSONObject(result);
+                    JSONObject c = jObject.getJSONObject("info");
+                    int faceCount = c.getInt("faceCount");
+                    if(faceCount==0){
+                        String[] tmp = new String[2];
+                        tmp[0]="인식불가";
+                        tmp[1]="0";
+                        return tmp;
+                    }
+                    else{
+                        JSONArray celebrities = jObject.getJSONArray("faces");
+                        JSONObject celebrity = celebrities.getJSONObject(0);
+                        JSONObject target = celebrity.getJSONObject("celebrity");
+                        String[] tmp = new String[2];
+                        tmp[0]=target.getString("value");
+                        tmp[1]=String.valueOf(target.getDouble("confidence"));
+                        return tmp;
+                    }
+                } else {
+                    System.out.println("error !!!");
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String[] s) {
+            super.onPostExecute(s);
+            asyncDialog.dismiss();
+            resultTextView.setText(s[0] + "   "+s[1]);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+        super.onPause();
+    }
+
+    @SuppressLint("NewApi")
+    private String getPath(Uri uri) throws URISyntaxException {
+        final boolean needToCheckUri = Build.VERSION.SDK_INT >= 19;
+        String selection = null;
+        String[] selectionArgs = null;
+        // Uri is different in versions after KITKAT (Android 4.4), we need to
+        // deal with different Uris.
+        if (needToCheckUri && DocumentsContract.isDocumentUri(getApplicationContext(), uri)) {
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                return Environment.getExternalStorageDirectory() + "/" + split[1];
+            } else if (isDownloadsDocument(uri)) {
+                final String id = DocumentsContract.getDocumentId(uri);
+                uri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+            } else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+                if ("image".equals(type)) {
+                    uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+                selection = "_id=?";
+                selectionArgs = new String[] {
+                        split[1]
+                };
+            }
+        }
+        if ("content".equalsIgnoreCase(uri.getScheme())) {
+            String[] projection = {
+                    MediaStore.Images.Media.DATA
+            };
+            Cursor cursor = null;
+            try {
+                cursor = getContentResolver()
+                        .query(uri, projection, selection, selectionArgs, null);
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                if (cursor.moveToFirst()) {
+                    return cursor.getString(column_index);
+                }
+            } catch (Exception e) {
+            }
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
+    }
+
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+}
